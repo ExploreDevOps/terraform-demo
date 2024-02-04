7fee5f2ba1a58b18900d75438fbc2fb7b7f2ed43
ReddiPams <rsp.mar85@gmail.com>
2024-02-04 16:35:50 +0530

    added files

---
+++ providers.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "trusty-obelisk-412501-tfstate"
+    prefix = "terraform/state"
+  }
+}

9d71fa25a5fed20b58b2990995652bda709409e4
ReddiPams <rsp.mar85@gmail.com>
2024-02-12 20:20:54 +0530

    added new main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+/*terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      #version = "<=5.15.0"
+      version = "<4.51.0"
+    }
+  }
+}*/
+
+provider "google" {
+    project = "trusty-obelisk-412501"
+    region = "us-central1"
+    credentials  = "key.json"
+  
+}
+
+#for_each example
+resource "google_storage_bucket" "my-bkt0" {
+    for_each = toset(["1","2","4","5"])
+    name = "trusty-obelisk-412501-test-${each.key}-reddi"
+    location = "US"
+  
+}
+#count example
+resource "google_storage_bucket" "my-bkt0" {
+    count = 5
+    name = "trusty-obelisk-412501-test-${count.index}"
+    location = "US"
+  
+}
+
+
